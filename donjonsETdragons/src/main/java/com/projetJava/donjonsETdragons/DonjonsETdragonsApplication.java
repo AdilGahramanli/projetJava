a71a9e5640c078062884bea03ec21f9843db8cc0
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2023-11-24 14:39:59 +0100

    DD

---
+++ donjonsETdragons/src/main/java/com/projetJava/donjonsETdragons/DonjonsETdragonsApplication.java
@@ -0,0 +1,13 @@
+package com.projetJava.donjonsETdragons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonjonsETdragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonjonsETdragonsApplication.class, args);
+	}
+
+}
---
+++ donjonsETdragons/src/test/java/com/projetJava/donjonsETdragons/DonjonsETdragonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetJava.donjonsETdragons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonjonsETdragonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
